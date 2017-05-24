9c2dad5c712c42dee907a42a79e9b60a49714032
1000pilar <nunua7a8@gmailcom>
2017-05-24 13:10:52 +0700

    stracture searching masih perlu pemahaman lebih jauh

--- api.js
+++ api.js
@@ -2,7 +2,7 @@ var express = require('express')
 var router = express.Router()
 var index = require('./index')
 
-router.get('/search', index.search)
+router.get('/search/:search', index.search)
 
 
 module.exports = router
--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 var express = require('express')
 var OAuth = require('oauth');
-
+require('dotenv').config()
 
 module.exports = {
   search : (req, res)=>{
     var oauth = new OAuth.OAuth(
     'https://api.twitter.com/oauth/request_token',
     'https://api.twitter.com/oauth/access_token',
-    'KaA05UHIwKgNlxVvLAOf3vwfE',
-    'Fbavai2xv2arnHsWk8CbXE4gGQXGQ7PtlNtMiNN4wp0GAiT113',
+    process.env.APP_KEY,
+    process.env.CONS_KEY,
     '1.0A',
     null,
     'HMAC-SHA1'
     );
     oauth.get(
-      'https://api.twitter.com/1.1/trends/place.json?id=23424977',
-      '858113611-AdO1SGcpa78RC4m6wtrjOFdu2SDqFu1llLb3Ojji', //test user token
-      'KSVonOBnG00QraKdcVLdNMiuJiqbBcvvbsKMTNB68BTaF', //test user secret
+      `https://api.twitter.com/1.1/search/tweets.json?q=%40${req.params.search}`,
+      process.env.TOKE_KEY, //test user token
+      process.env.SEC_KEY, //test user secret
       function (e, data){
         if (e) console.error(e);
         // console.log(require('util').inspect(data));
